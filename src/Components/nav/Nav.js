33060f53f66b8c1284ed0323d60ca1df4b5cce7c
leeseohoo <84429852+leeseohoo@users.noreply.github.com>
2023-02-02 15:01:39 +0900

    Add files via upload

--- src/Components/nav/Nav.js
+++ src/Components/nav/Nav.js
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from './logo.png';
+import { Link } from "react-router-dom";
 
 const Nav = () => {
     return (
@@ -9,8 +10,8 @@ const Nav = () => {
                     <img className="porfile-img" src ={logo} alt="profile"/>
                 </div>
                 <br/>
-                <h3 className="click">ABOUT</h3>
-                <h3 className="click">MEMBER</h3>
+                <Link to="/Section1"><h3 className="click">ABOUT</h3></Link> |{" "}
+                <Link to="/Section2"><h3 className="click">MEMBER</h3></Link>
                 <h3 className="click">CONTACT</h3>
                 <h3 className="click">INFORMATION</h3>
             </div>
